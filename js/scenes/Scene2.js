f68a5eebe85669a4551676740bf68274dddd9177
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-12 18:43:56 +0100

    Player now moves up and down the screen

--- js/scenes/Scene2.js
+++ js/scenes/Scene2.js
@@ -15,12 +15,11 @@ class Scene2 extends Phaser.Scene {
 
 
         // Add player image for testing
-        //this.player = this.add.image(config.width - 180, config.height / 2, 'player');
-        this.player = this.add.sprite(config.width - 180, config.height / 2, 'player');
+        this.player = this.physics.add.sprite(config.width - 180, config.height - 200, 'player');
         this.player.play('player_idle');
 
         // Stop player from going off the screen
-        //this.player.setCollideWorldBounds(true);
+        this.player.setCollideWorldBounds(true);
 
         // Make variable to listen for cursor keys
         this.cursorKeys = this.input.keyboard.createCursorKeys();
@@ -35,21 +34,23 @@ class Scene2 extends Phaser.Scene {
 
     movePlayer() {
         /**
-        * So every time the left arrow key is pressed we adjust
-        * the speed of the players with a negative value, so they move
-        * to the left
+        * So every time the up arrow key is pressed we adjust
+        * the speed of the player with a negative value, so they move
+        * up the screen but we also don't want the player going off 
+        * the foreground so they can't go higher than 320px,  
+        * "this.player.y > 320"
         * 
-        * else if the right key is pressed move to the right
+        * else if the down key is pressed move to the down
         * 
         * else set the X velocity to 0 to stop the ship from moving
         */
-        // if (this.cursorKeys.left.isDown) {
-        //     this.player.setVelocityY(-gameSettings.playerSpeed);
-        // } else if (this.cursorKeys.right.isDown) {
-        //     this.player.setVelocityY(gameSettings.playerSpeed)
-        // } else {
-        //     this.player.setVelocityY(0)
-        // }
+        if (this.cursorKeys.up.isDown && this.player.y > 320) {
+            this.player.setVelocityY(-gameSettings.playerSpeed);
+        } else if (this.cursorKeys.down.isDown) {
+            this.player.setVelocityY(gameSettings.playerSpeed)
+        } else {
+            this.player.setVelocityY(0)
+        }
     }
 
 
