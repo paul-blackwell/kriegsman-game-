78018b971af7f34400f2599aee52c835ca79e155
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-12 13:11:07 +0100

    Project setup

---
+++ js/game.js
@@ -0,0 +1,22 @@
+const config = {
+    width: 256,
+    height: 272,
+    backgroundColor: 0x000000,
+    scene: [Scene1, Scene2],
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    }
+}
+
+const gameSettings = {
+    playerSpeed: 200,
+}
+
+
+
+
+const game = new Phaser.Game(config, gameSettings);
