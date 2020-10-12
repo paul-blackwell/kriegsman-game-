cfc35b0a9748fedc87a78aeec379528e4b06d238
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-12 18:23:47 +0100

    Player now uses a sprite

--- js/scenes/Scene1.js
+++ js/scenes/Scene1.js
@@ -6,12 +6,26 @@ class Scene1 extends Phaser.Scene {
 
     // Preload all Images an spites 
     preload(){
-        this.load.image('player', 'assets/images/krieg-guardsman2.png');
+        this.load.image('foreground', 'assets/images/foreground.png');
+
+        this.load.spritesheet('player', 'assets/spritesheets/krieg-guardsman-shritesheet.png', {
+            frameWidth: 160,
+            frameHeight: 192,
+        });
     }
 
     create() {
         this.add.text(20, 20, "Loading game... ");
         this.scene.start('playGame');
+
+        // Basic 2 frame looping animation of the player
+        this.anims.create({
+            key: 'player_idle',
+            frames: this.anims.generateFrameNumbers('player'), // using the frames from the player sprite sheet
+            frameRate: 16,
+            repeat: -1
+        })
+
     }
 
 
--- js/scenes/Scene2.js
+++ js/scenes/Scene2.js
@@ -5,9 +5,53 @@ class Scene2 extends Phaser.Scene {
     }
 
     create() {
-       // Add player image for testing
-       this.player = this.add.image(config.width/ 2 - 50, config.height/ 2, 'player')
+
+        //. Just for testing
+        this.cameras.main.setBackgroundColor('#040C06');
+
+
+        // Add foreground
+        this.foreground = this.add.image(this.cameras.main.width / 2, config.height - 210, 'foreground')
+
+
+        // Add player image for testing
+        //this.player = this.add.image(config.width - 180, config.height / 2, 'player');
+        this.player = this.add.sprite(config.width - 180, config.height / 2, 'player');
+        this.player.play('player_idle');
+
+        // Stop player from going off the screen
+        //this.player.setCollideWorldBounds(true);
+
+        // Make variable to listen for cursor keys
+        this.cursorKeys = this.input.keyboard.createCursorKeys();
+
     }
+
+    update() {
+        // This function will control the players movement
+        this.movePlayer();
+    }
+
+
+    movePlayer() {
+        /**
+        * So every time the left arrow key is pressed we adjust
+        * the speed of the players with a negative value, so they move
+        * to the left
+        * 
+        * else if the right key is pressed move to the right
+        * 
+        * else set the X velocity to 0 to stop the ship from moving
+        */
+        // if (this.cursorKeys.left.isDown) {
+        //     this.player.setVelocityY(-gameSettings.playerSpeed);
+        // } else if (this.cursorKeys.right.isDown) {
+        //     this.player.setVelocityY(gameSettings.playerSpeed)
+        // } else {
+        //     this.player.setVelocityY(0)
+        // }
+    }
+
 
 
 }
