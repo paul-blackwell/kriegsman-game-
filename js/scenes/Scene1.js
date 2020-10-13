55cf3d5e76f68a9aa308ba311eabf76dc628df09
Paul Blackwell <blackwellpaul34@gmail.com>
2020-10-13 10:37:31 +0100

    Add player walking animation

--- js/scenes/Scene1.js
+++ js/scenes/Scene1.js
@@ -8,20 +8,37 @@ class Scene1 extends Phaser.Scene {
     preload(){
         this.load.image('foreground', 'assets/images/foreground.png');
 
+
+        // Spitesheet for player Idle
         this.load.spritesheet('player', 'assets/spritesheets/krieg-guardsman-shritesheet.png', {
             frameWidth: 160,
             frameHeight: 192,
         });
+
+        // Spitesheet for player Walking
+        this.load.spritesheet('playerWalking', 'assets/spritesheets/krieg-guardsman-animated-walking-sprite.png', {
+            frameWidth: 160,
+            frameHeight: 192,
+        });
+
     }
 
     create() {
         this.add.text(20, 20, "Loading game... ");
         this.scene.start('playGame');
 
-        // Basic 2 frame looping animation of the player
+        // Animation of the player idle
         this.anims.create({
             key: 'player_idle',
             frames: this.anims.generateFrameNumbers('player'), // using the frames from the player sprite sheet
+            frameRate: 16,
+            repeat: -1
+        })
+
+        // Animation of the player walking
+        this.anims.create({
+            key: 'player_walking',
+            frames: this.anims.generateFrameNumbers('playerWalking'), // using the frames from the player sprite sheet
             frameRate: 16,
             repeat: -1
         })
--- js/scenes/Scene2.js
+++ js/scenes/Scene2.js
@@ -33,23 +33,45 @@ class Scene2 extends Phaser.Scene {
 
 
     movePlayer() {
+
+
+        /**
+         * This will change the player texture to a
+         * walking sprite if they are moving, Might refactor this
+         * later to use a boolean 
+         */
+        const makePlayerWalk = () => {
+            if(this.player.texture.key === 'player') {
+                this.player.setTexture('playerWalking');
+                this.player.play('player_walking');
+           }
+        }
+
         /**
         * So every time the up arrow key is pressed we adjust
         * the speed of the player with a negative value, so they move
         * up the screen but we also don't want the player going off 
         * the foreground so they can't go higher than 320px,  
-        * "this.player.y > 320"
+        * "this.player.y > 320", then play walking animation
         * 
         * else if the down key is pressed move to the down
+        * then play walking animation
         * 
         * else set the X velocity to 0 to stop the ship from moving
+        * then play idle animation
         */
         if (this.cursorKeys.up.isDown && this.player.y > 320) {
             this.player.setVelocityY(-gameSettings.playerSpeed);
+            makePlayerWalk()
         } else if (this.cursorKeys.down.isDown) {
             this.player.setVelocityY(gameSettings.playerSpeed)
+            makePlayerWalk()
         } else {
             this.player.setVelocityY(0)
+            if(this.player.texture.key === 'playerWalking') {
+                this.player.setTexture('player');
+                this.player.play('player_idle');
+           }
         }
     }
 
