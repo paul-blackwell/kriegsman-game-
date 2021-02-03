93fccdb21ae742a29c0870c23d88e1ac4d2a57a4
Paul Blackwell <blackwellpaul34@gmail.com>
2021-02-03 11:02:01 +0000

    Made a change to Webpack config file  as build was failing

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,6 @@ const path = require('path');
 module.exports = {
     entry: {
         main: "./src/index.js",
-        vendor: "./src/vendor.js"
     },
     plugins: [],
     devtool: 'eval-source-map',
